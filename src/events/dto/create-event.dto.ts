3aa7aa42a58108140a44ae63dccca3132ce61029
Luis Martínez <lemartinezmendoza@gmail.com>
2023-11-15 16:10:08 -0500

    docs: add swagger annotations in events controllers

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,13 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateEventDto {
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
+  @ApiProperty({ description: 'Event name', example: 'This is an event name' })
   name: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(40)
+  @ApiProperty({
+    description: 'Event description',
+    example: 'This is an event description',
+  })
   description: string;
 }
--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -16,14 +16,24 @@ import { EventsService } from './events.service';
 import { Event } from './entities/event.entity';
 import { AuthGuard } from '../auth/auth.guard';
 import { JwtUser } from 'src/users/interfaces/user.interface';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('events')
 @UsePipes(new ValidationPipe())
+@ApiTags('Events')
 export class EventsController {
   constructor(private eventsService: EventsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
+  @ApiCreatedResponse({ description: 'Event created successfully' })
+  @ApiBadRequestResponse({ description: 'Something went wrong' })
   async createEvent(
     @Body() createEventDto: CreateEventDto,
     @Request() req: Request & { user: JwtUser },
@@ -35,6 +45,10 @@ export class EventsController {
   }
 
   @Get()
+  @ApiOkResponse({ description: 'Events successfully obtained' })
+  @ApiBadRequestResponse({ description: 'Something went wrong' })
+  @ApiQuery({ name: 'p', required: false, description: 'Page number' })
+  @ApiQuery({ name: 'epp', required: false, description: 'Elements per page' })
   async getEvents(
     @Param('p', new DefaultValuePipe(1), ParseIntPipe) page: number,
     @Param('epp', new DefaultValuePipe(5), ParseIntPipe)
