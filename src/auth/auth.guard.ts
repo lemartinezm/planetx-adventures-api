a1239783970d8e2ea82b4477d1ce3149003510e5
Luis Martínez <lemartinezmendoza@gmail.com>
2023-11-16 12:18:50 -0500

    chore: add auth or guest guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -34,3 +34,34 @@ export class AuthGuard implements CanActivate {
     return type === 'Bearer' ? token : undefined;
   }
 }
+
+@Injectable()
+export class AuthOrGuestGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractToken(request);
+    if (!token) {
+      request.user = null;
+      return true;
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.SECRET_JWT,
+      });
+
+      // Add user info to request object
+      request.user = payload;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    return true;
+  }
+
+  private extractToken(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
