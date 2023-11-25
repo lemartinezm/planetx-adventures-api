c10e829127e88d659fe52a1c0bb4caf6406d980b
Luis Martínez <lemartinezmendoza@gmail.com>
2023-11-24 21:47:25 -0500

    fix: update cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.FRONT_URL],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('PlanetX Adventures API')
